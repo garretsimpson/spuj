66fd08654576bf787dbcf5a3c89289983d2ef4b4
FatcatX <garretsimpson@yahoo.com>
2024-05-23 21:23:47 -0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+/**
+ * Main
+ */
+public class Main {
+
+  static String VERSION = "SPU 1.0";
+
+  public static void main(String[] args) {
+    System.out.println(VERSION);
+    System.out.println(new Date());
+  }
+
+}
